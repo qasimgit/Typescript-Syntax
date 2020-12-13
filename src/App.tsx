1006a6ab857f642d3f28e3e64603f5eaacd43a0e
qasimgit <qasimpkkhi@gmail.com>
2020-12-13 13:10:59 +0500

    typesctript

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, useState , FC } from 'react';
 import './App.css';
 
-function App() {
+interface AppProps {
+sendSearchQuery(): void;
+}
+
+// type AppProps = {
+// sendSearchQuery(): void;
+// }
+const App: FC<AppProps> = ({sendSearchQuery}) => {
+  const [searchValue , setSearchValue] = useState<string>();
+
+  const onSearchInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  }
+
+  const search = () => {
+    sendSearchQuery();
+  }
+
   return (
     <div>
       <h1>
         Typscript Learning
       </h1>
+      <input type="text" onChange={onSearchInput} name="search" value={searchValue} />
+      <button onClick={search} >Search</button>
     </div>
   )
 }
